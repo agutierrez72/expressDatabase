d71e66385b4582c319a8f85ee6ba2b780d9c9e39
agutierrez72 <agutierrez72@ucmerced.edu>
2020-03-29 00:07:05 -0700

    initialize user routes

--- routes/userAccount.js
+++ routes/userAccount.js
@@ -3,8 +3,21 @@ const router = express.Router()
 const User = require('../models/userAccount')
 
 // All Users Route
-router.get('/', (req, res) => {
-    res.render('userAccounts/index')
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if(req.query.name != null && req.query.name !=''){
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try{
+        const users = await User.find(searchOptions)
+        res.render('userAccounts/index', { 
+            users: users,
+            searchOptions: req.query
+        })
+    } catch{
+        res.redirect('/')
+    }
+    
 })
 
 // New User Route
@@ -13,8 +26,33 @@ router.get('/new', (req, res) => {
 })
 
 // Create User Route
-router.post('/', (req, res) => {
-    res.send('Create')
+router.post('/', async (req, res) => {
+    const user = new User({
+        name: req.body.firstName +" "+ req.body.lastName,
+        username: req.body.userName,
+        password: req.body.password2
+    })
+    try{
+        const newUser = await user.save()
+        res.redirect('users')
+    } catch{
+        res.render('userAccounts/new', {
+            user: user,
+            errorMessage: 'Error creating User'
+        })
+    }
+    // user.save((err, newUser) => {
+    //     if(err){
+    //         res.render('userAccounts/new', {
+    //             user: user,
+    //             errorMessage: 'Error creating User'
+    //         })
+    //     } else{
+    //         // res.redirect('users/${newUser.id}')
+    //         res.redirect('users')
+    //     }
+    // })
+    
 })
 
 module.exports = router
