2b9beaba572bd32b5247b962c3ff2c3cdb2bfbed
agutierrez72 <agutierrez72@ucmerced.edu>
2020-03-28 22:50:58 -0700

    new endpoint for userAccounts

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/index')
+const userRouter = require('./routes/userAccount')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -22,5 +23,6 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/users', userRouter)
 
 app.listen(process.env.PORT || 3000)
